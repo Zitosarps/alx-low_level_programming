e133cb36c1d786295b34735c67dbff3dd94a86e8
Zitosarps <kizitosarpey1@gmail.com>
2023-07-12 23:51:10 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_calloc - function with two argument
+ * @nmemb: member of block
+ * @size: size of memory
+ *
+ * Description: allocates memory for an array
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *ptr;
+	void *done;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < (size * nmemb); i++)
+		ptr[i] = 0;
+	done = ptr;
+	return (done);
+}
