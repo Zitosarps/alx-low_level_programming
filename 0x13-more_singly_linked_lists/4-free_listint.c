2326cb6d958dda17c1065a3278dc5e6a0cb92aa5
Zitosarps <kizitosarpey1@gmail.com>
2023-08-01 11:48:30 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,8 +10,9 @@ void free_listint(listint_t *head)
 
 	while (head)
 	{
-	       temp = head->next;
-	       free(head);
-	       head = temp;
+		temp = head->next;
+		free(head);
+		head = temp;
 	}
 }
+
