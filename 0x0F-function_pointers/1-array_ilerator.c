1bd9017685e1ef285e1aec136e9287ceb5b29a3d
Zitosarps <kizitosarpey1@gmail.com>
2023-08-01 19:50:56 -0700

    1-array_ilerator.c

---
+++ 0x0F-function_pointers/1-array_ilerator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+  * array_iterator - ...
+  * @array: ...
+  * @size: ...
+  * @action: ...
+  *
+  * Return: ...
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (array != NULL && action != NULL && size > 0)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+}
